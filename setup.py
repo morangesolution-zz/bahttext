b10c3b400c28dc6e9ab1744833a19ae9b8b27741
Seksan Poltree <seksan@morange.co.th>
2014-05-31 20:18:49 +0700

    change layout and add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bahttext',
+      version='1.0',
+      description='Tool for convert currency number to Thai text',
+      author='Seksan Poltree',
+      author_email='seksan@morange.co.th',
+      url='https://github.com/morangesolution/bahttext',
+      download_url='https://github.com/morangesolution/bahttext/tarball/master',
+      packages=['bahttext'],
+      license="BSD",
+     )
+
